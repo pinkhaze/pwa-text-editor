c70b322ce86ca7f83f0702708583366630593495
Monika <raciam@oregonstate.edu>
2023-09-19 22:47:55 -0500

    Deploying to Heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   try {
     const jateDb = await openDB('jate', 1);
@@ -26,8 +26,7 @@ export const putDb = async (content) => {
   }
 };
 
-
-// TODO: Add logic for a method that gets all the content from the database
+// Logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('Get from the database')
   const jateDb = await openDB('jate', 1)
