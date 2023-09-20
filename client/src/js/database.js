2492b775250337a5657d11beb786697cbf9b0035
Monika <raciam@oregonstate.edu>
2023-09-19 20:37:41 -0500

    Set up GET method for content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,18 +14,31 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
-  const result = await request;
-  console.log('Content added to database', result);
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+    console.log('Content added to database', result);
+  } catch (err) {
+    console.error('putDb not implemented');
+  }
+};
 
-}
-//console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Get from the database')
+  const jateDb = await openDB('jate', 1)
+  const tx = jateDb.transaction('jate', 'readonly')
+  const store = tx.objectStore('jate')
+  const request = store.get(1)
+  const result = await request
+  console.log('result value', result)
+}
+
+//console.error('getDb not implemented');
 
 
 initdb();
